dbca7651660227fbce068032258386e309c0c3b8
Ahmed <ahmed.abdelaal1921@mail.com>
2021-02-20 00:26:58 +0200

    wip

--- resources/views/components/multiselect.blade.php
+++ resources/views/components/multiselect.blade.php
@@ -12,13 +12,12 @@
                                          x-text="option[title]"></div>
                                     <div class="flex flex-auto flex-row-reverse">
                                         <div x-on:click.stop="remove(option)">
-                                            <svg class="fill-current h-4 w-4 " role="button" viewBox="0 0 20 20">
-                                                <path d="M14.348,14.849c-0.469,0.469-1.229,0.469-1.697,0L10,11.819l-2.651,3.029c-0.469,0.469-1.229,0.469-1.697,0
-                                               c-0.469-0.469-0.469-1.229,0-1.697l2.758-3.15L5.651,6.849c-0.469-0.469-0.469-1.228,0-1.697s1.228-0.469,1.697,0L10,8.183
-                                               l2.651-3.031c0.469-0.469,1.228-0.469,1.697,0s0.469,1.229,0,1.697l-2.758,3.152l2.758,3.15
-                                               C14.817,13.62,14.817,14.38,14.348,14.849z"/>
+                                            <svg class="w-4 h-4 cursor-pointer" fill="none" stroke="currentColor"
+                                                 viewBox="0 0 24 24"
+                                                 xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                      d="M6 18L18 6M6 6l12 12"></path>
                                             </svg>
-
                                         </div>
                                     </div>
                                 </div>
@@ -32,8 +31,16 @@
 
                             <button type="button"
                                     class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                                <x-heroicon-o-chevron-down x-show="show" x-on:click="close"/>
-                                <x-heroicon-o-chevron-up x-show="!show" x-on:click="open"/>
+                                <svg x-show="show" x-on:click="close" fill="none" stroke="currentColor"
+                                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M19 9l-7 7-7-7"></path>
+                                </svg>
+                                <svg x-show="!show" x-on:click="open" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M5 15l7-7 7 7"></path>
+                                </svg>
                             </button>
                         </div>
                     </div>
